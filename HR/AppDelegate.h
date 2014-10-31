1da79451a75f39468bb3a4103382edba7da6cb32
Francisco Ramirez <franciscorjs@gmail.com>
2014-10-30 23:28:13 -0700

    Initial Commit

---
+++ HR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HR
+//
+//  Created by Home on 10/30/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HR
+//
+//  Created by Home on 10/30/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
