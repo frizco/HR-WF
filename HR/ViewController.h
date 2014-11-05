0b48ac42a35153a6f028c6d2b2080467dfec7df8
Francisco <franciscorjs@gmail.com>
2014-11-04 19:50:41 -0800

    first

--- HR/ViewController.h
+++ HR/ViewController.h
@@ -10,6 +10,20 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISwitch *antPlusSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *bluetoothSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *suuntoSwitch;
+
+@property (weak, nonatomic) IBOutlet UISwitch *wildcardSwitch;
+
+@property (weak, nonatomic) IBOutlet UIButton *connectButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *hrLabel;
+@property (weak, nonatomic) IBOutlet UILabel *searialLabel;
+
+
+- (IBAction)connectButtonTouched:(id)sender;
+
 
 @end
 
